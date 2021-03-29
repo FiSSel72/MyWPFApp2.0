57c8953f2b5607f2d6029698fea7843031879464
FiSSel <golovchenkoartem03@gmail.com>
2021-03-29 18:43:10 +0300

    Добавьте файлы проекта.

---
+++ MyWPFApp/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyWPFApp
+{
+    class ViewModel : ViewModelBase
+    {
+        #region Fields
+        private ICommand _loginCommand;
+        private string _login;
+        private string _password;
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand => _loginCommand ?? (_loginCommand = new RelayCommand(arg => Signin()));
+        #endregion
+
+        #region Properties
+        public string LoginStroke
+        {
+            get { return _login; }
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(LoginStroke));
+            }
+        }
+        public string PasswordStroke
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(PasswordStroke));
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private void Signin()
+        {
+            MessageBox.Show("aaa");
+        }
+        #endregion
+
+        #region Public Methods
+        #endregion
+    }
+}
